473bbad0d073f41a079bd1f908e6399b8a7dc710
toru hamaguchi <roo20f@gmail.com>
2014-04-21 03:00:31 +0900

    Use fish_plugins instead of `set -x PATH`

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,11 +7,10 @@ set fish_theme gitstatus
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
+set fish_plugins autojump brew node
 
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
-set -x PATH /usr/sbin /usr/local/sbin $PATH
-set -x PATH /usr/local/share/npm/bin $PATH
 
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
